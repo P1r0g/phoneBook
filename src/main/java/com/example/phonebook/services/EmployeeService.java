8234f6c208ae136de3025d10abffc4c650564294
holyasha <goou.price@gmail.com>
2025-11-30 17:30:15 +0300

    Обновление сервисов и репозиториев

--- src/main/java/com/example/phonebook/services/EmployeeService.java
+++ src/main/java/com/example/phonebook/services/EmployeeService.java
@@ -3,14 +3,13 @@ package com.example.phonebook.services;
 import java.util.List;
 
 import com.example.phonebook.dto.AddEmployeeDto;
+import com.example.phonebook.dto.ShowEmployeeDto;
 
 public interface EmployeeService {
     
     void addEmployee(AddEmployeeDto employeeDto);
 
-    List<ShowEmployeeInfoDto> allEmployees();
-
-    ShowDetailedEmployeeInfoDto employeeInfo(String employeeFullName);
+    List<ShowEmployeeDto> allEmployees();
 
     void fireEmployee(String employeeFullName);
 }
--- src/main/java/com/example/phonebook/services/EmployeeServiceImpl.java
+++ src/main/java/com/example/phonebook/services/EmployeeServiceImpl.java
@@ -1,11 +1,16 @@
 package com.example.phonebook.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.phonebook.dto.AddEmployeeDto;
+import com.example.phonebook.dto.ShowEmployeeDto;
 import com.example.phonebook.models.entities.Employee;
 import com.example.phonebook.repositories.DepartmentRepository;
 import com.example.phonebook.repositories.EmployeeRepository;
@@ -15,25 +20,54 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @Transactional(readOnly = true)
-public class EmployeeServiceImpl {
-private final EmployeeRepository employeeRepository;
-private final DepartmentRepository departmentRepository;
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
 
-public EmployeeServiceImpl(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, ModelMapper mapper) {
-    this.employeeRepository = employeeRepository;
-    this.departmentRepository = departmentRepository;
-    this.mapper = mapper;
-    log.info("EmployeeServiceImpl инициализорован");
-}
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, ModelMapper mapper) {
+        this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
+        this.mapper = mapper;
+        log.info("EmployeeServiceImpl инициализорован");
+    }
 
-@Override
-@Transactional
-@CacheEvict(cacheNames = "employees", allEntries = true)
-public void addEmployee(AddEmployeeDto employeeDto) {
-    log.debug("Добавление нового сотрудника: {} {} {}", employeeDto.getLastName(), employeeDto.getMiddleName(), employeeDto.getFirstName());
+    @Override
+    @Transactional
+    @CacheEvict(cacheNames = "employees", allEntries = true)
+    public void addEmployee(AddEmployeeDto employeeDto) {
+        log.debug("Добавление нового сотрудника: {} {} {}", employeeDto.getLastName(), employeeDto.getMiddleName(), employeeDto.getFirstName());
 
-    Employee employee = mapper.map(employeeDto, Employee.class);
-    employee.setDepartment(departmentRepository.findById(employeeDto.getDepartmentId()));
-}
-    
-}
+        Employee employee = mapper.map(employeeDto, Employee.class);
+        employee.setDepartment(departmentRepository.findById(employeeDto.getDepartment()).orElse(null));
+
+        employeeRepository.saveAndFlush(employee);
+        log.info("Сотрудник успешно добавлен: {} {} {}", employeeDto.getLastName(), employeeDto.getMiddleName(), employeeDto.getFirstName());
+    }
+
+    @Override
+    @Cacheable(value = "employees", key = "'all'")
+    public List<ShowEmployeeDto> allEmployees() {
+        log.debug("Получение списка всех сотрудников");
+        List<ShowEmployeeDto> employees = employeeRepository.findAll().stream()
+                .map(employee -> mapper.map(employee, ShowEmployeeDto.class))
+                .collect(Collectors.toList());
+        log.debug("Найдено сотрудников: {}", employees.size());
+        return employees;
+        }
+    }
+    @Override
+    @Transactional
+    @CacheEvict(cacheNames = "employees", allEntries = true)
+    public void fireEmployee(String employeeFullName) {
+        log.debug("Увольнение сотрудника: {}", employeeFullName);
+
+        Employee employee = employeeRepository.findEmployeeByFullName(employeeFullName);
+        if (employee == null) {
+            log.warn("Попытка уволить несуществующего сотрудника: {}", employeeFullName);
+            //throw new EmployeeNotFoundException("Сотрудник с именем '" + employeeFullName + "' не найден");
+        }
+
+        employeeRepository.deleteEmployeeByFullName(employeeFullName);
+        log.info("Сотрудник уволен: {}", employeeFullName);
+    }
+}   
